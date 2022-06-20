b821da2f70a7f81745ad44bc0a2bcdd695ce8990
Joseph Onyilo <onyilo@gmail.com>
2022-06-20 14:46:38 +0100

    Group Admin List

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -4,7 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use \App\Models\Profile;
+use \App\Models\GroupMember;
+use \App\Models\User;
 
 class Group extends Model
 {
